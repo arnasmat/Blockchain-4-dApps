c686693634e371a8d1c5dc732886b9612d1d14bd
arnasmat <arnas.matuolis@gmail.com>
2025-11-29 16:00:36 +0200

    Change version for truffle to 0.8.21

--- contracts/CrowdSourcing.sol
+++ contracts/CrowdSourcing.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.21;
 /*
     Crowdsourcing with milestones, expected logic:
     - There is a general system owner, who owns the crowdsourcing "page"
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.21;
 
 // taken from https://github.com/fabiojose/ethereum-ex/blob/master/contracts/Migrations.sol
 contract Migrations {
